ead78d81373d62e7f6108ff3274243fdee66f248
Bryan Karaffa <bryankaraffa@gmail.com>
2015-03-16 14:27:40 -0700

    Added recipe metadata

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures rsc_skeleton_cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "rightscale"
+
+recipe "rsc_skeleton_cookbook::default",
+  "Default Recipe"
